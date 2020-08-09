45c78e92b1991ec7554071c794489e82990fbc13
rinsuki <428rinsuki+git@gmail.com>
2020-08-10 08:25:11 +0900

    iCloud Drive にフォルダができるように

--- Checklists/Document.swift
+++ Checklists/Document.swift
@@ -9,7 +9,17 @@
 import Cocoa
 import SwiftUI
 
-class Document: NSDocument {
+class Check: Codable {
+    var checked: Bool
+    let title: String
+}
+
+class DocumentContent: Codable {
+    var checks = [Check]()
+}
+
+class Document: NSDocument, Codable {
+    var content = DocumentContent()
 
     override init() {
         super.init()
@@ -38,14 +48,16 @@ class Document: NSDocument {
     override func data(ofType typeName: String) throws -> Data {
         // Insert code here to write your document to data of the specified type, throwing an error in case of failure.
         // Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let encoder = JSONEncoder()
+        return try encoder.encode(self.content)
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
         // Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
         // Alternatively, you could remove this method and override read(from:ofType:) instead.
         // If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let decoder = JSONDecoder()
+        self.content = try decoder.decode(DocumentContent.self, from: data)
     }
 
 
