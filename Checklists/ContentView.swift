59079f9aa27f71e55bb51f8f887b8b2f0ceaebe2
rinsuki <428rinsuki+git@gmail.com>
2020-08-10 07:32:51 +0900

    Initial Commit

---
+++ Checklists/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Checklists
+//
+//  Created by user on 2020/08/10.
+//  Copyright © 2020 rinsuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
